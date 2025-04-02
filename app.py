a744e0734ce1ce4f6fcb3bbb984731d48af79534
Guru Chandra <guru.chandra@dreamvu.com>
2025-04-02 12:26:45 +0530

    test ci cd pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from my CI/CD powered Flask app! can you see me??--chandra'
+    return 'Hello from my CI/CD powered Flask app! can you see me??--chandra--Hola'
 
 if __name__ == '__main__': 
     app.run(debug=True, host='0.0.0.0',port=5000)
